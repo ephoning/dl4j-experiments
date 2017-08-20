a78fafca026ce77895964078f5437b3700457a99
Edgar Honing <ephoning@gmail.com>
2017-08-20 14:35:07 -0600

    add odd-even test project

---
+++ dl4jtst2-odd-even/src/main/java/org/eddie/dl4j/Main.java
@@ -0,0 +1,97 @@
+package org.eddie.dl4j;
+
+import org.deeplearning4j.nn.conf.Updater;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import java.io.File;
+
+/**
+ * attempt 8-bit odd/even detection
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int seed = 123;
+        double learningRate = 0.01;
+        int batchSize = 20; //50;
+        int nEpochs = 15; //30;
+
+        int numInputs = 8;
+        int numOutputs = 2;
+        int numHiddenNodes = 12; //20 is overkill ; 12 is sufficient
+
+        final String filenameTrain  = new ClassPathResource("/classification/odd_even_data_train.csv").getFile().getPath();
+        final String filenameTest  = new ClassPathResource("/classification/odd_even_data_eval.csv").getFile().getPath();
+
+        //Load the training data:
+        RecordReader rr = new CSVRecordReader();
+        rr.initialize(new FileSplit(new File(filenameTrain)));
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,0,2);
+
+        //Load the test/evaluation data:
+        RecordReader rrTest = new CSVRecordReader();
+        rrTest.initialize(new FileSplit(new File(filenameTest)));
+        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,0,2);
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(1)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(learningRate)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.SOFTMAX).weightInit(WeightInit.XAVIER)
+                        .nIn(numHiddenNodes).nOut(numOutputs).build())
+                .pretrain(false).backprop(true).build();
+
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(new ScoreIterationListener(10));  //Print score every 10 parameter updates
+
+        System.out.println("Train model....");
+        long trainStart = System.currentTimeMillis();
+        for ( int n = 0; n < nEpochs; n++) {
+            model.fit( trainIter );
+        }
+        long trainEnd = System.currentTimeMillis();
+        System.out.println(String.format("Model training took %d millis.", trainEnd - trainStart));
+
+        System.out.println("Evaluate model....");
+        Evaluation eval = new Evaluation(numOutputs);
+        while(testIter.hasNext()){
+            DataSet t = testIter.next();
+            INDArray features = t.getFeatures();
+            INDArray labels = t.getLabels();
+            INDArray predicted = model.output(features,false);
+
+            eval.eval(labels, predicted);
+        }
+
+        //Print the evaluation statistics
+        System.out.println(eval.stats());
+    }
+}
